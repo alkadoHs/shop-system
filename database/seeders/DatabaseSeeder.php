1f880a4ebb434793e922d6974f608771f191cd33
alkadoHs <alkadosichone@gmail.com>
2024-11-26 10:47:52 +0300

    demo seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Branch;
+use App\Models\Company;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,32 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
+        $company = Company::create([
+            'name' => 'Test Company',
             'email' => 'test@example.com',
+            'phone' => '07123456789',
+            'address' => '123 Main St',
         ]);
+
+        $branch = Branch::create([
+            'name'=> 'Main Branch',
+            'company_id' => $company->id,
+        ]);
+
+        User::create([
+            'name' => 'Test User',
+            'email' => 'test@mauzodata.com',
+            'phone' => '07123456789',
+            'email_verified_at' => now(),
+            'password' => \Illuminate\Support\Facades\Hash::make('password'),
+            'remember_token' => \Illuminate\Support\Str::random(10),
+            'company_id' => $company->id,
+            'branch_id' => $branch->id,
+        ]);
+
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
