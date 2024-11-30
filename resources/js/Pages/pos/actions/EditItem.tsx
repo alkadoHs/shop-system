23d6ced89692b7d929d35267cfb6173b21ba5936
alkadoHs <alkadosichone@gmail.com>
2024-11-30 07:42:47 +0300

    ...

--- resources/js/Pages/pos/actions/EditItem.tsx
+++ resources/js/Pages/pos/actions/EditItem.tsx
@@ -68,7 +68,7 @@ const EditItem = ({ item }: { item: cartItem }) => {
                         if (result) {
                             setImei(result.text);
                             setStopStream(true);
-                            setTimeout(() => setOpen(false), 0);
+                            setTimeout(() => setOpen(false), 1000);
                         } else setImei("Not Found");
                     }}
                     imei={imei}
